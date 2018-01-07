157a1f81a725d5f4a6a7db07fe498f4873f75e56
Jamie Howard <jamie@morrowmade.com>
2018-01-06 23:23:33 -0500

    Added typed.js for typing animation.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,3 @@
-var words = ['hackers', 'doers', 'dreamers', 'inventors', 'builders', 'artists'],
-    i = 0;
-
 function finish() {
     $("#cta-switcher").html('innovators')
 
@@ -10,29 +7,22 @@ function finish() {
 
     $("#morrowmade-lead").fadeTo(1500, 0.1, function() {
         $(this).removeClass('text-info').addClass('text-warning');    
-    }).fadeTo(3300, 1);
+    }).fadeTo(2500, 1);
     
     $(".logo").fadeTo(1500, 0.1, function() {
         $(this).css('box-shadow', '0px 0px 25px #ffc107').css('border-color', '#ffc107').css('filter', 'sepia()');
-    }).fadeTo(3300, 1);
+    }).fadeTo(2500, 1);
 }
 
-function changeWord() {
-    if (i == words.length) {
-        finish();    
-    } else {
-        $("#cta-switcher").html(words[i]);
-        i++;
-        delayChange();
-    }
-}
+$(function(){
 
-function delayChange() {
-    setTimeout(function() {
-        changeWord();
-    }, 1300);
-}
+    $("#cta-switcher").typed({
+        strings: ['makers', 'art', 'artists', 'hackers', 'innovators'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        callback : function () {
+            finish()
+        }
+    });
 
-setTimeout(function() {
-    delayChange();
-}, 1000);
+});
